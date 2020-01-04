1585083471060a0cd64d062ee8346d6068d5fc03
TomyAnchundia <53131409+TomyAnchundia@users.noreply.github.com>
2020-01-03 19:46:58 -0500

    Add files via upload

---
+++ PrototypePattern/Clase/Estudiante.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern.Clase
+{
+    public class Estudiante : ICloneable
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string identidad;
+
+        public string Identidad
+        {
+            get { return identidad; }
+            set { identidad = value; }
+        }
+
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+          //  throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PrototypePattern.Clase;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prototype Pattern");
+
+            Estudiante estudiante1 = new Estudiante() { Nombre = "Tomy", Identidad = "1313364414", Edad = 22 };  //instanciamos un estudiante
+            Estudiante estudianteClonado = estudiante1;        //Se clona un estudiante con los mismos datos    
+            estudianteClonado.Edad = 25;                       // cambiamos la edad del estudiante clonado y los cambios afectaran al estudianclonado y al original
+
+            /* LO CORRECTO PARA IMPLEMENTAR LA CLONACION  */
+            Estudiante estudianteClonado = estudiante1.Clone() as Estudiante;  //el cambio de la edad solo se provocara en el clone
+            Console.WriteLine(estudiante1.Nombre);
+
+        }
+    }
+}
